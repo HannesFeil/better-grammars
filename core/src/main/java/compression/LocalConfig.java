a0e1246b54b35389fe68e86da1da3b23e6536bdc
HannesFeil <hannesfeil@gmx.de>
2025-08-03 19:40:44 +0200

    Update GIT_ROOT for new directory structure

--- core/src/main/java/compression/LocalConfig.java
+++ core/src/main/java/compression/LocalConfig.java
@@ -6,6 +6,6 @@ package compression;
 public class LocalConfig {
 
 	// TODO Change this to your local path of the git repository clone
-	public static final String GIT_ROOT = ".";
+	public static final String GIT_ROOT = "../";
 }
 
